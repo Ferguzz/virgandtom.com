f38daf56878bdc21ed3eb5d6bd7f38c217ddb29e
Tom Myers <tom.stephen.myers@gmail.com>
2014-05-21 15:16:53 +0100

    Static files served from within app engine.

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
                  <html>
                      <body>
                      <div style="margin-right:auto;margin-left:auto;width:640px">
-                         <img src="https://s3-us-west-1.amazonaws.com/tommyersphotos/virgandtom.jpg" height="480px" width="640px">
+                         <img src="img/virgandtom.jpg" height="480px" width="640px">
                          </div>
                      </body>
                  </html>`
